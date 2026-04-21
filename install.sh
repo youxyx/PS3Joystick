cfca760b337cd021c9d804fd8554d0cde96c65bd
youxyx <35842445+youxyx@users.noreply.github.com>
2026-04-22 02:01:58 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env sh
 
 if [ "$(uname)" == "Darwin" ]; then
-	echo "ds4drv doesn't work on Mac OS!"
-	echo "try installing pygame (commented out in this script) and running mac_joystick.py"
+	echo "本驱动未适配Mac OS!"
 	exit 0
 fi
 
 FOLDER=$(dirname $(realpath "$0"))
 cd $FOLDER
 
+# 安装PS3原生手柄依赖
+sudo apt update
+sudo apt install -y python3-pygame
+sudo pip3 install pygame
 
-#pygame is no longer needed!
-# sudo apt-get install -y libsdl-ttf2.0-0
-# yes | sudo pip3 install pygame
+# 配置手柄/蓝牙权限（必须）
+sudo usermod -aG input pi
+sudo usermod -aG bluetooth pi
 
-yes | sudo pip3 install ds4drv
+# 安装项目本体
 sudo python3 setup.py clean --all install
 
-exit
-# we don't want the example joystick service installed by default
-
+# 注册服务
 for file in *.service; do
 	[ -f "$file" ] || break
-	sudo ln -s $FOLDER/$file /lib/systemd/system/
+	sudo ln -sf $FOLDER/$file /lib/systemd/system/
 done
-
 
 sudo systemctl daemon-reload
